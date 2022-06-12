66bb219ee71bb662d0292e91397fb584a3b0a8e8
aarkue <aaron.kuesters@rwth-aachen.de>
2022-06-12 16:09:09 +0000

    Fix: Correct place removal (also for end and start) and respect doNotRemoveStartEndPlaces option
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/AlphaRevisitExperiments/Trunk@46356 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/alpharevisitexperiments/util/ReplayProcessor.java
+++ src/org/processmining/alpharevisitexperiments/util/ReplayProcessor.java
@@ -16,12 +16,13 @@ public class ReplayProcessor {
     public final static String FREQUENT_VARIANT_OPTION_ID = "replay_frequent_variants";
     public final static String DO_NOT_REMOVE_STARTEND_PLACES_OPTION_ID = "do_not_remove_startend_places";
     public final static ExperimentOption[] STANDARD_REPLAY_OPTIONS = {
-            new ExperimentOption<>(Integer.class,FREQUENT_VARIANT_OPTION_ID,"top% of frequent variants to use for replay and place removal",0,0,100),
-            new ExperimentOption<>(Boolean.class,DO_NOT_REMOVE_STARTEND_PLACES_OPTION_ID,"Do not remove places that are in the start or end marking.",false),
+            new ExperimentOption<>(Integer.class, FREQUENT_VARIANT_OPTION_ID, "top% of frequent variants to use for replay and place removal", 0, 0, 100),
+            new ExperimentOption<>(Boolean.class, DO_NOT_REMOVE_STARTEND_PLACES_OPTION_ID, "Do not remove places that are in the start or end marking.", false),
 //            new ExperimentOption<>(Boolean.class,"try_connect","Try to connect disconnected transitions",false),
     };
+
     public static String[] getTopVariants(HashMap<String, Integer> allVariantsWithCaseNumbers, int totalNumberOfCases,
-                                           int goalMinCasePercentage) {
+                                          int goalMinCasePercentage) {
         String[] variantArray = allVariantsWithCaseNumbers.keySet().toArray(new String[0]);
         Arrays.sort(variantArray, Comparator.comparingInt(e -> -allVariantsWithCaseNumbers.getOrDefault(e, 0)));
         int numberOfCoveredCases = 0;
@@ -35,9 +36,11 @@ public class ReplayProcessor {
                 + (numberOfCoveredCases * 100.0 / totalNumberOfCases) + "% of all cases.");
         return Arrays.copyOfRange(variantArray, 0, lastVariantIndex + 1);
     }
-    public static void replayAndRemovePlaces(AcceptingPetriNet net, String[] frequentVariants){
-        replayAndRemovePlaces(net,frequentVariants,false);
+
+    public static void replayAndRemovePlaces(AcceptingPetriNet net, String[] frequentVariants) {
+        replayAndRemovePlaces(net, frequentVariants, false);
     }
+
     public static void replayAndRemovePlaces(AcceptingPetriNet net, String[] frequentVariants, boolean doNotRemoveStartEndPlaces) {
         HashMap<String, Transition> transitionMap = new HashMap<>();
         for (Transition t : net.getNet().getTransitions()) {
@@ -70,7 +73,6 @@ public class ReplayProcessor {
                         } else {
                             System.out.println("No token in place" + p.getLabel() + " -> remove place");
                             placesToRemove.add(p);
-//                            net.getNet().removePlace(p);
                         }
                     } else {
                         System.err.println("Error: Incoming PN node for transition is not a place?!");
@@ -91,29 +93,35 @@ public class ReplayProcessor {
                 }
             }
             for (Place p : net.getNet().getPlaces()) {
-
                 if (net.getFinalMarkings().contains(p)) {
                     if (tokenCount.get(p) != 1) {
-                        System.out.println("No token in End place");
+                        System.out.println("No token in End place: " + p.getLabel());
+                        placesToRemove.add(p);
                     }
                 } else {
                     if (tokenCount.get(p) != 0) {
-                        System.out.println("Token left in place" + p.getLabel() + " -> remove place");
+                        System.out.println("Token left in place " + p.getLabel() + " -> remove place");
                         placesToRemove.add(p);
                     }
                 }
             }
 
-            for(Marking m : net.getFinalMarkings()){
-                for(Place p : m){
+            if (doNotRemoveStartEndPlaces) {
+                for (Marking m : net.getFinalMarkings()) {
+                    for (Place p : m) {
+                        placesToRemove.remove(p);
+                    }
+                }
+                for (Place p : net.getInitialMarking()) {
                     placesToRemove.remove(p);
                 }
             }
-            for(Place p : net.getInitialMarking()){
-                placesToRemove.remove(p);
-            }
 
             for (Place p : placesToRemove) {
+                for (Marking m : net.getFinalMarkings()) {
+                    m.remove(p);
+                }
+                net.getInitialMarking().remove(p);
                 net.getNet().removePlace(p);
             }
         }
