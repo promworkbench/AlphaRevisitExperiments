8a9b44e75d96c9b4a289f16812490677ad2d9ed7
aarkue <aaron.kuesters@rwth-aachen.de>
2022-06-12 16:07:00 +0000

    Added initial version of a2.1
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/AlphaRevisitExperiments/Trunk@46355 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/alpharevisitexperiments/algorithms/AlphaTwoDotOneWithReplay.java
@@ -0,0 +1,27 @@
+package org.processmining.alpharevisitexperiments.algorithms;
+
+import org.deckfour.xes.model.XLog;
+import org.processmining.acceptingpetrinet.models.AcceptingPetriNet;
+import org.processmining.alpharevisitexperiments.options.ExperimentOption;
+import org.processmining.alpharevisitexperiments.util.ReplayProcessor;
+import org.processmining.contexts.uitopia.UIPluginContext;
+
+public class AlphaTwoDotOneWithReplay extends AlgorithmExperiment{
+
+
+    final static ExperimentOption[] options = ReplayProcessor.STANDARD_REPLAY_OPTIONS;
+    final AlphaTwoDotOne alphaTwoDotOne = new AlphaTwoDotOne();
+
+    public AlphaTwoDotOneWithReplay() {
+        super("Alpha 2.1 with replay",options);
+    }
+
+
+    @Override
+    public AcceptingPetriNet execute(UIPluginContext context, XLog log) {
+        AcceptingPetriNet net = alphaTwoDotOne.execute(context,log);
+        String[] frequentVariants = ReplayProcessor.getTopVariants(alphaTwoDotOne.getLogProcessor().getVariants(), log.size(),this.getOptionValueByID(ReplayProcessor.FREQUENT_VARIANT_OPTION_ID));
+        ReplayProcessor.replayAndRemovePlaces(net, frequentVariants,this.getOptionValueByID(ReplayProcessor.DO_NOT_REMOVE_STARTEND_PLACES_OPTION_ID));
+        return net;
+    }
+}
